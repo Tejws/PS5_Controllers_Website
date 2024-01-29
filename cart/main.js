3c6b85e47a43052b818b0653fd6916f437657fd3
Tejas Ingle <154742112+Tejws@users.noreply.github.com>
2024-01-29 19:11:25 +0530

    Add files via upload

---
+++ cart/main.js
@@ -0,0 +1,140 @@
+const openShopping = document.querySelector(".shopping"),
+      closeShopping = document.querySelector(".closeShopping"),
+      body = document.querySelector("body"),
+      list= document.querySelector(".list"),
+      listCard = document.querySelector(".listCard"),
+      total = document.querySelector(".total"),
+      quantity = document.querySelector(".quantity")
+
+
+openShopping.addEventListener("click", () => {
+    body.classList.add("active");
+})
+
+closeShopping.addEventListener("click", () => {
+    body.classList.remove("active")
+})
+
+let products = [
+    {
+        "id": 1,
+        "name": "PRODUCT 1",
+        "image":"d.png",
+        "price": 2000
+    },
+    {
+        "id": 2,
+        "name": "PRODUCT 2",
+        "image":"c.png",
+        "price": 2200
+    },
+    {
+        "id": 3,
+        "name": "PRODUCT 3",
+        "image":"b.png",
+        "price": 2400
+    },
+    {
+        "id": 4,
+        "name": "PRODUCT 4",
+        "image":"a.png",
+        "price": 2600
+    },
+    {
+        "id": 5,
+        "name": "PRODUCT 5",
+        "image":"9.png",
+        "price": 1400
+    },
+    {
+        "id": 6,
+        "name": "PRODUCT 6",
+        "image":"e.png",
+        "price": 1800
+    },
+    {
+        "id": 7,
+        "name": "PRODUCT 7",
+        "image":"7.png",
+        "price": 1800
+    },
+    {
+        "id": 8,
+        "name": "PRODUCT 8",
+        "image":"8.png",
+        "price": 1800
+    }
+]
+
+
+let listCards = [];
+
+const initApp = () => {
+    products.forEach((value, key) => {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
+        newDiv.innerHTML = `
+            <img src = "img/${value.image}">
+            <div class = "title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick = "addToCard(${key})">Add To Card</button>
+        `;
+        list.appendChild(newDiv)
+    })
+}
+
+initApp()
+
+
+const addToCard = key => {
+    if(listCards[key] == null) {
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        // console.log(listCards);
+        listCards[key].quantity = 1;
+        // console.log(listCards[key].quantity);
+    }
+
+    reloadCard()
+}
+
+const reloadCard = () => {
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice= 0;
+
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price
+        count = count + value.quantity;
+
+        if(value != null) {
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+                <div><img src = "img/${value.image}"></div>
+                <div class = "cardTitle">${value.name}</div>
+                <div class = "cardPrice">${value.price.toLocaleString()}</div>
+
+                <div>
+                    <button style = "background-color:#560bad;" class = "cardButton" onclick = "changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class = "count">${value.quantity}</div>
+                    <button style = "background-color:#560bad;" class = "cardButton" onclick = "changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
+            `
+            listCard.appendChild(newDiv)
+        }
+
+        total.innerText = totalPrice.toLocaleString();
+        quantity.innerText = count;
+    })
+}
+
+
+const changeQuantity = (key, quantity) => {
+    if(quantity == 0) {
+        delete listCards[key]
+    }
+    else {
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price
+    }
+    reloadCard()
+}
